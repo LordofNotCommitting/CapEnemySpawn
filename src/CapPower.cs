7b150875fd86a1de2d2bf9803161a7cd858ec5d5
LordofNotCommitting <shadymcshade247@gmail.com>
2025-10-30 19:11:07 -0400

    first commit

---
+++ src/CapPower.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using static System.Collections.Specialized.BitVector32;
+
+namespace CapEnemySpawn
+{
+
+    [HarmonyPatch(typeof(MissionFactory), nameof(MissionFactory.InitDropPoints))]
+    public class CapPower
+    {
+        //steam mod ID 3594238447
+        static int power_Cap = Plugin.ConfigGeneral.ModData.GetConfigValue<int>("Faction_Missiongen_Power_Cap", 1);
+        //[HarmonyPatch(typeof(WoundSlotRecord), "ImplantSocketsDefault", MethodType.Getter)]
+        static Logger temp_log = new Logger();
+        public static void Postfix(ref MissionFactory __instance, ref Mission mission, Faction victim)
+        {
+            Difficulty difficulty = __instance._state.Get<Difficulty>();
+            MissionDifficultyRecord missionDifficultyRecord = Data.MissionDifficulty.Get(mission.MissionDifficulty);
+            PrizeByRatingRecord prizeByRatingRecord = Data.PrizesByRatings.Get(mission.ProcMissionType, mission.MissionDifficulty);
+
+            //temp_log.Log("Mission gen faction power detected as " + victim.Power +". Setting it as " + power_Cap);
+            float temp_power = Mathf.Min(victim.Power, (float)power_Cap);
+
+            int monstersPointsLimit = Mathf.RoundToInt(((float)missionDifficultyRecord.MonsterPointsPerStage + temp_power * prizeByRatingRecord.PowerToMonsterPointsMult) * difficulty.Preset.MonsterPoints);
+            foreach (KeyValuePair<string, DungeonGenerationPlan> keyValuePair in mission.LocationPlans)
+            {
+                keyValuePair.Value.MonstersPointsLimit = monstersPointsLimit;
+            }
+
+
+        }
+    }
+}
+
+
---
+++ src/ModConfigGeneral.cs
@@ -0,0 +1,30 @@
+﻿using MGSC;
+using ModConfigMenu.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CapEnemySpawn
+{
+    // Token: 0x02000006 RID: 6
+    public class ModConfigGeneral
+    {
+        // Token: 0x0600001D RID: 29 RVA: 0x00002840 File Offset: 0x00000A40
+        public ModConfigGeneral(string ModName, string ConfigPath)
+        {
+            this.ModName = ModName;
+            this.ModData = new ModConfigData(ConfigPath);
+            this.ModData.AddConfigHeader("STRING:General Settings", "general");
+            this.ModData.AddConfigValue("general", "about", "STRING:<color=#f51b1b>New missions</color> will have their  enemy count capped as if their power is X (if their power is higher than X).\n");
+            this.ModData.AddConfigValue("general", "Faction_Missiongen_Power_Cap", 30000, 500, 100000, "STRING:Set Mission Faction Power Cap", "STRING:New missions will have their enemy count capped as if their power is X (if their power is higher than X).");
+            this.ModData.AddConfigValue("general", "about2", "STRING:<color=#f51b1b>The game must be restarted after setting then saving this config to take effect.</color>\n");
+            this.ModData.RegisterModConfigData(ModName);
+        }
+        // Token: 0x04000011 RID: 17
+        private string ModName;
+
+        // Token: 0x04000012 RID: 18
+        public ModConfigData ModData;
+
+    }
+}
